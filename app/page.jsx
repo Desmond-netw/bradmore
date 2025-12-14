c9de8d68ca78379a9140936013e312cf6ac94420
Desmond-netw <ddeveh1@outlook.com>
2025-12-14 15:03:58 +0000

    added all page sections to homepage

--- app/page.jsx
+++ app/page.jsx
@@ -3,7 +3,13 @@
 import About from "@/components/About";
 import Header from "@/components/Header";
 import Hero from "@/components/Hero";
+import Stats from "@/components/Stats";
+import Work from "@/components/Work";
+import Services from "@/components/Services";
 import Topbar from "@/components/Topbar";
+import Faq from "@/components/Faq";
+import Footer from "@/components/Footer";
+import Contacts from "@/components/Contacts";
 
 const Home = () => {
   return (
@@ -12,6 +18,12 @@ const Home = () => {
       <Header />
       <Hero />
       <About />
+      <Stats />
+      <Services />
+      <Work />
+      <Faq />
+      <Contacts />
+      <Footer />
     </div>
   );
 };

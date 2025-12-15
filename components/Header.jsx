95604ee0862b6d0116da533169cd64f8e3859e49
Desmond-netw <ddeveh1@outlook.com>
2025-12-15 05:44:19 +0000

    modify navigation links ,with hover effect and transition CTA

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,4 +1,5 @@
 "use client";
+import { RiArrowRightUpLine } from "react-icons/ri";
 // imports
 import { Link as ScrollLink } from "react-scroll";
 
@@ -29,25 +30,56 @@ export const Header = () => {
   return (
     <header className="bg-primary py-4 sticky top-0 z-50">
       <div className="container mx-auto ">
-        <ul className="flex gap-12 text-white capitalize">
-          {links.map((link, indx) => {
-            return (
-              <li className="font-semibold px-4 py-2 rounded hover:text-brand-yellow hover:opacity-90">
-                <ScrollLink
-                  to={link.path}
-                  smooth
-                  spy
-                  className="cursor-pointer"
-                  activeClass="text-accent"
+        <div className="w-full flex items-center justify-between">
+          {/* logo */}
+          <div>logo</div>
+          {/* Navigation */}
+          <nav className="hidden xl:flex items-center gap-12">
+            <ul className="flex ">
+              {links.map((link, index) => (
+                <li
+                  key={index}
+                  className="
+                  text-white
+                  text-sm
+                  font-semibold
+                  px-2 py-1
+                  border-b-2 border-transparent
+                  hover:border-brand-yellow
+                  hover:text-brand-yellow
+                  uppercase
+                  tracking-[1.2px]
+                  transition
+                  after:content-['/'] after:mx-4 after:text-accent 
+                "
                 >
-                  {link.name}
-                </ScrollLink>
-              </li>
-            );
-          })}
-        </ul>
+                  <ScrollLink
+                    to={link.path}
+                    smooth
+                    spy
+                    className="cursor-pointer"
+                    activeClass="text-accent border-brand-yellow"
+                  >
+                    {link.name}
+                  </ScrollLink>
+                </li>
+              ))}
+            </ul>
+
+            {/* CTA Button */}
+            <button className=" group w-[200px] min-w-[200px] h-[54px] px-6 flex items-center justify-center bg-white rounded hover:opacity-90 ">
+              <div className="flex-1 text-center text-primary font-primay font-bold text-sm uppercase">
+                get a quote
+              </div>
+              <div className="w-11 h-11 bg-primary flex items-center justify-center">
+                <RiArrowRightUpLine className="text-white text-xl group-hover:rotate-45 transition-all duration-200" />
+              </div>
+            </button>
+          </nav>
+        </div>
       </div>
     </header>
   );
 };
+
 export default Header;

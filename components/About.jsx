9d822dd421d830f9d885719d543b2174fcb738b5
Desmond-netw <ddeveh1@outlook.com>
2025-12-18 05:09:50 +0000

    modify about layout

--- components/About.jsx
+++ components/About.jsx
@@ -1,7 +1,16 @@
 export const About = () => {
   return (
-    <div id="about" className="h-screen bg-red-200">
-      About
+    <div id="about" className="pt-16 xl:pt-32 ">
+      <div className="container mx-auto">
+        <div className="flex flex-col gap-12 xl:gap-0 xl:flex-row xl:items-center">
+          {/* Prerttier */}
+          <div className="flex-1">
+            <div> About Us</div>
+          </div>
+          {/* Image */}
+          <div className="flex-1">Image</div>
+        </div>
+      </div>
     </div>
   );
 };

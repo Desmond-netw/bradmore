5195a1e92c4d2b64c956b4b999f30dd618b69aa7
Desmond-netw <dsquare200@gmail.com>
2026-01-17 18:28:36 +0200

    Initilize the testimonial layout

--- components/Testimonials.jsx
+++ components/Testimonials.jsx
@@ -1,5 +1,25 @@
+
+import Image from "next/image"
+import Button from "./lib/Button";
+import Pretitle from "./lib/Pretitle";
+
+
+
 const Testimonials = () => {
-  return <div>Testimonials</div>;
+  return (
+    <section className="pt-16 xl:pt-32">
+      <div className="container mx-auto">
+        <div>
+          {/*  ------- Text ------ */}
+          <div className="flex-1"></div>
+          {/* -------- Img & Slider ---- */}
+          <div className="flex-1"></div>
+        </div>
+      </div>
+
+    </section>
+
+  )
 };
 
 export default Testimonials;

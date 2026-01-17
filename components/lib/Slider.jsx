cad1cc660302c096c5492a8f805aab26251f3ca2
Desmond-netw <dsquare200@gmail.com>
2026-01-17 20:30:29 +0200

    added extral avatas

--- components/lib/Slider.jsx
+++ components/lib/Slider.jsx
@@ -6,6 +6,7 @@ import Image from "next/image";
 export const Slider = () => {
   return (
     <Swiper className="bg-white shadow-custom w-full  max-w-[630px] h-[200px]">
+      {/* --- SLIDER 1 */}
       <SwiperSlide>
         <div className="px-12 md:pl-[60px] flex items-center gap-9 h-full">
           {/* Avatar */}
@@ -28,6 +29,55 @@ export const Slider = () => {
           </div>
         </div>
       </SwiperSlide>
+      {/* SLIDER 2 */}
+      <SwiperSlide>
+        <div className="px-12 md:pl-[60px] flex items-center gap-9 h-full">
+          {/* Avatar */}
+          <div className="relative w-20 h-20 rounded-full overflow-hidden">
+            <Image
+              src="/assets/img/testimonials/avatar1.png"
+              fill
+              alt="Client avatar"
+              className="object-cover"
+              quality={100}
+            />
+          </div>
+
+          {/* text */}
+          <div className="flex-1 xl:max-w-[340px] flex flex-col gap-2">
+            <p className="text-sm opacity-80 font-primary">
+              Excellent service and professional delivery.
+            </p>
+            <h5 className="font-semibold">John Doe</h5>
+          </div>
+        </div>
+      </SwiperSlide>
+      {/* SLIDER 3 */}
+       <SwiperSlide>
+        <div className="px-12 md:pl-[60px] flex items-center gap-9 h-full">
+          {/* Avatar */}
+          <div className="relative w-20 h-20 rounded-full overflow-hidden">
+            <Image
+              src="/assets/img/testimonials/avatar2.jpg"
+              fill
+              alt="Client avatar"
+              className="object-cover"
+              quality={100}
+            />
+          </div>
+
+          {/* text */}
+          <div className="flex-1 xl:max-w-[340px] flex flex-col gap-2">
+            <p className="text-sm opacity-80 font-primary">
+              Excellent service and professional delivery.
+            </p>
+            <h5 className="font-semibold">John Doe</h5>
+          </div>
+        </div>
+      </SwiperSlide>
+
+    {/* slider btns */}
+     
     </Swiper>
   );
 };

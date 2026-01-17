e36b8a34dd83286d80dbf03979dc85b67c9cd02f
Desmond-netw <dsquare200@gmail.com>
2026-01-17 20:21:55 +0200

    added slider 1

---
+++ components/lib/Slider.jsx
@@ -0,0 +1,35 @@
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import "swiper/css/navigation";
+import Image from "next/image";
+
+export const Slider = () => {
+  return (
+    <Swiper className="bg-white shadow-custom w-full  max-w-[630px] h-[200px]">
+      <SwiperSlide>
+        <div className="px-12 md:pl-[60px] flex items-center gap-9 h-full">
+          {/* Avatar */}
+          <div className="relative w-20 h-20 rounded-full overflow-hidden">
+            <Image
+              src="/assets/img/testimonials/avatar.png"
+              fill
+              alt="Client avatar"
+              className="object-cover"
+              quality={100}
+            />
+          </div>
+
+          {/* text */}
+          <div className="flex-1 xl:max-w-[340px] flex flex-col gap-2">
+            <p className="text-sm opacity-80 font-primary">
+              Excellent service and professional delivery.
+            </p>
+            <h5 className="font-semibold">John Doe</h5>
+          </div>
+        </div>
+      </SwiperSlide>
+    </Swiper>
+  );
+};
+
+export default Slider;

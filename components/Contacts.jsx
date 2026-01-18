328ce86d51de144d91d56cffee01cdd95c892c70
Desmond-netw <dsquare200@gmail.com>
2026-01-18 23:36:54 +0200

    initilize and imported components for contracts section

--- components/Contacts.jsx
+++ components/Contacts.jsx
@@ -1,7 +1,21 @@
+
+import {RiChat1Line, RiMap2Line, RiSmartphoneLine} from "react-icons/ri";
+import Socials from "./lib/Socials"
+
+
 const Contacts = () => {
   return (
-    <div id="contact" className="h-screen bg-purple-200">
-      Contacts
+    <div id="contact" className="pt-16 xl:pt-32">
+      <div className="container mx-au">
+        <div>
+          <div className="flex flex-col xl:flex-row">
+            {/* -- info ------ */}
+            <div className="w-full">Info</div>
+            {/* -- form---- */}
+            <div className="flex-1">Form</div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
